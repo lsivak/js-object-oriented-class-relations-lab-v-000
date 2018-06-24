4a77a4284a5b67f63eaa2674a83f9d416c862aed
Lara Sanders <larasanders@gmail.com>
2018-06-24 18:08:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let driverId = 0
 class Driver {
   constructor(name) {
     this.name = name
-    this.id = ++driverId
+    this.id = driverId++
     store.drivers.push(this)
   }
 }
@@ -11,13 +11,13 @@ let passengerId = 0
   class Passenger {
     constructor(name) {
       this.name = name
-      this.id = ++passengerId
+      this.id = passengerId++
       store.passengers.push(this)
     }
   }
   let tripId = 0
   class Trip {
-    constuctor(driver, passenger) {
+    constuctor(driver, passenger) {  
       this.driverId = driver.id
       this.passengerId = passenger.id
       this.id = tripId++
