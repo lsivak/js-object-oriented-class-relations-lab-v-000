011aa3d833b00c91fd183b17a1c80a0464d666ca
Lara Sanders <larasanders@gmail.com>
2018-06-24 18:58:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ let passengerId = 0
     constuctor(driver, passenger) {
       this.driverId = driver.id
       this.passengerId = passenger.id
+      debugger
       this.id = tripId++
       debugger
       store.trips.push(this)
