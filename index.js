aec906525049c8a840a5ba4746e6705e35dab022
Lara Sanders <larasanders@gmail.com>
2018-06-24 19:06:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ class Driver {
   trips () {
     return store.trips.filter(trip => {
       return trip.driverId === this.id
+      return driver
     })
   }
   passengers () {
     return this.trips().map(trip => {
-      return trip.passenger
+      return trip.passenger()
     })
   }
 }
